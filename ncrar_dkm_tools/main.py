87da0a1d3426bee6b0ee492b0833dea84aa4ed98
Brad Buran <bradley.buran@va.gov>
2023-07-17 14:18:53 -0700

    Refactor SRM aggregate in preparation for new tool

--- ncrar_dkm_tools/main.py
+++ ncrar_dkm_tools/main.py
@@ -1,12 +1,12 @@
 from enaml.qt.qt_application import QtApplication
 import enaml
 with enaml.imports():
-    from .main_window import AggregationWindow
+    from .srm.main_window import SRMAggregationWindow
 
 
-def main_aggregate():
+def main_srm_aggregate():
     app = QtApplication()
-    view = AggregationWindow()
+    view = SRMAggregationWindow()
     view.show()
     app.start()
     return True
--- ncrar_dkm_tools/srm/util.py
+++ ncrar_dkm_tools/srm/util.py
@@ -1,3 +1,6 @@
+'''
+Utilities for aggregating SRM files for DKM lab
+'''
 import datetime as dt
 import json
 from pathlib import Path
@@ -6,16 +9,47 @@ import re
 import pandas as pd
 
 
+# Filename pattern expected of SRM files.
 p_filename = re.compile(r'PCO(?P<subject>\d{3})_(?P<visit>\d{3})_\d{3}_(?P<condition>\w+)(?P<date>(_\d{2}){6})')
 
 
 def parse_filename(filename):
+    '''
+    Parse subject, visit, condition and date out of filename
+
+    Parameters
+    ----------
+    filename : {str, pathlib.Path}
+        Filename to parse.
+
+    Returns
+    -------
+    info : dict
+        Dictionary with filename information as values. Date will be converted
+        to a Python datetime object.
+    '''
+    filename = Path(filename)
     file_info = p_filename.match(filename.stem).groupdict()
     file_info['date'] = dt.datetime.strptime(file_info['date'], '_%y_%m_%d_%H_%M_%S')
     return file_info
 
 
 def load_file(filename):
+    '''
+    Load header and data from file
+
+    Parameters
+    ----------
+    filename : {str, pathlib.Path}
+        Filename to parse.
+
+    Returns
+    -------
+    header : dict
+        Header information stored at top of file.
+    data : pd.DataFrame
+        Data in file.
+    '''
     filename = Path(filename)
     with filename.open() as fh:
         header = json.loads(fh.readline())
@@ -33,6 +67,25 @@ def _aggregate_file(filename):
 
 
 def aggregate(files):
+    '''
+    Aggregate all *.bgc files found in the list of files and folders
+
+    Parameters
+    ----------
+    files : list of pathlib.Path
+        Can be a mix of *.bcg files and directories. If a directory is found in
+        the list, it will be recursively scanned for all *.bgc files. All *.bgc
+        files found will be aggregated.
+
+    Returns
+    -------
+    results : pandas.DataFrame
+        DataFrame containing one row per file.
+    results_mean : pandas.DataFrame
+        DataFrame with TMR averaged across subject, visit and condition.
+    errors : list
+        List of errors when attempting to process *.bgc files
+    '''
     bgc_files = []
     for filename in files:
         if filename.suffix == '.bgc':
